910495551f971c082f4ef26e3d1498b9892d1ba1
Avaz <azizoov97@gmail.com>
2022-08-02 09:22:16 +0500

    tasbih codes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Tasbih from "./components/tasbih";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Tasbih />
+      </>
+    );
+  }
+}
+
+export default App;
