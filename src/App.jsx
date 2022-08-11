742f7dc48c348c1883ae7523fb6626cff383857f
Avaz <azizoov97@gmail.com>
2022-08-11 08:21:22 +0500

    tic tac toe codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { Component } from "react";
-import Tabs from "./components/tabs/tabs";
+import { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Tabs />
-      </>
-    );
-  }
+import Board from "./components/board/Board";
+import Info from "./components/info/Info";
+import "./app.css";
+
+function App() {
+  const [reset, setReset] = useState(false);
+
+  const [winner, setWinner] = useState("");
+
+  const resetBoard = () => {
+    setReset(true);
+  };
+
+  return (
+    <div className="App">
+      <div className={`winner ${winner !== "" ? "" : "shrink"}`}>
+        <div className="winner-text">{winner}</div>
+
+        <button onClick={() => resetBoard()}>Reset Board</button>
+      </div>
+
+      <Board
+        reset={reset}
+        setReset={setReset}
+        winner={winner}
+        setWinner={setWinner}
+      />
+      <Info />
+    </div>
+  );
 }
 
 export default App;
